2b9d0fb87a75b392e8bc966e4d57d0a7a0c2ae49
SArie_ <lethaldrum97@gmail.com>
2024-03-23 18:11:11 +0530

    CharJS Setup

--- app/views.py
+++ app/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from .models import Profile
 
-# Create your views here.
+
 def view_chart(request):
-    return render(request,"app/index.html")
+    labels = []
+    data = []
+    queryset = Profile.objects.order_by("-age")  # Order by name ascending
+
+    for person in queryset:
+        labels.append(person.name)
+        data.append(person.age)
+
+    context = {
+        "labels": labels,
+        "data": data
+    }
+
+    print("labels:", labels)
+    print("data:", data)
+
+    return render(request, "app/index.html", context)
